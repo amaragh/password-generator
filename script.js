9b631008c6e07a77e988248af89f561cb4c45b97
amaragh <mercer.alecia@gmail.com>
2022-08-28 14:12:47 -0500

    add comments and remove console.log entries

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// function to generate a password based on user selected criteria for length and character type
 var generatePassword = function () {
-
+  // determine the password length and set to a variable
   var length = passwordLength();
 
+  // initialize strings that will be used to source the characters for the password
   var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerChars = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "0123456789";
@@ -24,6 +26,7 @@ var generatePassword = function () {
   // initialize array to store the strings from which the password characters will be selected
   var passwordSources = [];
 
+  // call function to ask user which character types they wans to have included in the password
   charTypeSelection.charTypePrompts();
 
   // if user opted to include a character type, add the string of corresponding characters to the passwordSources array
@@ -40,28 +43,24 @@ var generatePassword = function () {
     passwordSources.push(specialChars);
   }
 
-  console.log(passwordSources.length);
-
   // initialize empty string which will store the completed password
   var password = "";
 
+  // randomly add characters to the password until the length matches what the user selected
   for (i = 0; i < length; i++) {
     // find index of the source to be used for character selection
     var sourceIndex = Math.floor(Math.random() * passwordSources.length);
-    console.log("SOURCE INDEX IS " + sourceIndex);
 
     // identify the source of the character selection
     var source = passwordSources[sourceIndex];
-    console.log("SOURCE IS " + source);
 
     // find index of the character within the identified source
     var charPosition = Math.floor(Math.random() * source.length);
-    console.log("CHARACTER POSITION IS " + charPosition);
 
-    // locate the character within the source and append it to the password
+    // locate the character within the source and append it to the password string
     password += source[charPosition];
-    console.log("PASSWORD IS " + password);
   }
+  return password;
 }
 
 
@@ -69,7 +68,6 @@ var generatePassword = function () {
 var passwordLength = function () {
   // prompt the user for the password length
   var promptLength = window.prompt("Please enter a password length from 8 to 128 characters.")
-  console.log("Password length = " + promptLength)
 
   // validate whether the entered value is a number from 8 to 128
   if (!(promptLength >= 8) || !(promptLength <= 128)) {
@@ -82,23 +80,18 @@ var passwordLength = function () {
   }
 }
 
+// object to collect user selections for character type when prompted
 var charTypeSelection = {
   lower: "",
   upper: "",
   numeric: "",
   special: "",
   charTypePrompts: function () {
+    // prompt for yes or no the each chacarter type then store the answer as the new object value
     this.lower = window.prompt("Include lowercase characters? Enter Y for YES and N for NO");
-    // console.log("Incude lower? " + this.lower);
-
     this.upper = window.prompt("Include uppercase characters? Enter Y for YES and N for NO");
-    // console.log("Incude upper? " + this.upper);
-
     this.numeric = window.prompt("Include numeric characters? Enter Y for YES and N for NO");
-    // console.log("Incude numeric? " + this.numeric);
-
     this.special = window.prompt("Include special characters? Enter Y for YES and N for NO");
-    // console.log("Incude special? " + this.special);
 
     // convert values to upper case
     this.lower = this.lower.toUpperCase();
@@ -109,31 +102,8 @@ var charTypeSelection = {
     // check if user selected yes to include any of the character types 
     if (!(Object.values(this).indexOf('Y') > -1)) {
       window.alert("You did not include any of the characters types. You must select least one type to generate a password.")
+      // run the function again to allow user to select character types again
       return this.charTypePrompts();
     }
   }
 }
-
-
-
-// console.log("Password length = " + passwordLength());
-// charTypeSelection.charTypePrompts();
-// console.log(charTypeSelection);
-// console.log(charTypeSelection.lower);
-// console.log(charTypeSelection.upper);
-// console.log(charTypeSelection.numeric);
-// console.log(charTypeSelection.special);
-
-// console.log(generatePassword());
-
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
